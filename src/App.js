1ff9ca5e02caf6d859fecbda42852511bd10feb6
niteshthapa <nitesh_thapa@ymail.com>
2023-04-13 16:58:18 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react';
+import Employee from './feature/Employee/Employee';
+import Search from './components/ui/search/Search';
+function App() {
+  const [employees, setEmployees] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentSelection, setCurrentSelection] = useState("");
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        setLoading(true)
+        const response = await fetch("https://retoolapi.dev/KShrmC/data")
+        const data = await response.json();
+        setEmployees(data);
+        setLoading(false)
+      } catch (error) {
+        setLoading(false)
+        alert("Something goes wrong")
+      }
+    }
+    fetchData()
+
+  }, []);
+  const searchCurrentUser = (data) => {
+    setCurrentSelection(data)
+  }
+  return (
+    <div className='container'>
+      {loading && "Loading..."}
+      {currentSelection.length > 0}
+      <Search searchCurrentUser={searchCurrentUser} />
+      <Employee employees={employees.filter((item, index) => (
+        currentSelection.length > 0 ? item.employee.toLowerCase().substr(0, currentSelection.length) === currentSelection.toLowerCase() : item.employee
+      ))} />
+    </div>
+  );
+}
+export default App;
